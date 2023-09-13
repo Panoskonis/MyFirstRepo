33805cd56588073b519291091f6c5c343f90a1b3
Panagiotis Konis <panoskonis15@gmail.com>
2023-09-13 16:17:52 +0300

    Initial Commit

---
+++ RUN.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Compile your C program
+gcc Graph.c -o Graph.exe
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful."
+
+    # Run your C program
+    ./Graph.exe
+
+    # Check if the program exited successfully
+    if [ $? -eq 0 ]; then
+        
+        dot -Tpng Test.dot -o output.png 
+        dot -Tpng Test1.dot -o output1.png 
+    else
+        echo "Program execution failed."
+    fi
+else
+    echo "Compilation failed."
+fi
